6b59a6ad97593f66e6c0cd875be0414e4e3c6477
Dante Evans <110344184+RaidenIV@users.noreply.github.com>
2026-04-16 18:15:34 -0400

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -14,7 +14,8 @@ const JWT_EXPIRES_IN = '30d';
 // ─── Middleware ───────────────────────────────────────────────────────────────
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '6mb' }));
+app.use(express.urlencoded({ extended: true, limit: '6mb' }));
 app.use(express.static(path.join(__dirname, '..', 'client')));
 
 // ─── MongoDB ──────────────────────────────────────────────────────────────────
